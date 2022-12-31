5e4f51c9ae585e9a2fa8204101985f9265d2c4ae
dev-064 <divyanshujha112@gmail.com>
2023-01-01 02:32:14 +0530

    .

--- project_back_end/index.js
+++ project_back_end/index.js
@@ -3,16 +3,45 @@ const { spawn } = require("child_process");
 const scrapy = require("node-scrapy");
 const fetch = require("node-fetch");
 const mysql = require("./connection").con;
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
-app.use(cors({
-  origin: '*'
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 const port = 3300;
 const modifySearchParamter = (searchParameter) => {
   var separatedArray = searchParameter.split(" ");
   return separatedArray.join("%20");
+};
+const scrapeData = (searchParameter,url,model) => {
+  fetch(url)
+    .then((res) => res.text())
+    .then((body) => {
+      const content = scrapy.extract(body, model);
+      let qry1='delete from newsarticles';
+      mysql.query(qry1,(err,result)=>{
+        if(err){
+          console.log(err)
+        }
+        else{
+          console.log("success deleting")
+        }
+      })
+      content.news_articles.map((article) => {
+        let qry = `insert into newsarticles values (0,'${searchParameter}','${article.news_title.replaceAll(`'`,``)}','${"https://news.google.com/"+article.news_link}','${article.news_imageLink}')`;
+        mysql.query(qry, (err, result) => {
+          if (err) {
+            console.log(err)
+            return
+          } else {
+            console.log("success")
+          }
+        });
+      });
+    });
 };
 app.post("/", (req, res) => {
   const { searchParameter } = req.body;
@@ -27,12 +56,20 @@ app.post("/", (req, res) => {
         news_imageLink: "a figure img (src)",
       },
     ],
+
   };
-  fetch(url)
-    .then((res) => res.text())
-    .then((body) => {
-      res.send(scrapy.extract(body,model).news_articles[0].news_title)
-    });
+  scrapeData(searchParameter,url,model)
+  res.send("complete");
+});
+app.post("/getArticles", (req, res) => {
+  let qry = `select * from newsarticles where NewsSearchParameter='${req.body.searchParameter}'`;
+  mysql.query(qry, (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
 });
 app.listen(port, () =>
   console.log(`Example app listening on port 
