93870dff71ccd422cd68b5b23fd55dc024e84f61
dev-064 <divyanshujha112@gmail.com>
2023-01-03 16:09:25 +0530

    .

--- project_back_end/index.js
+++ project_back_end/index.js
@@ -4,6 +4,7 @@ const scrapy = require("node-scrapy");
 const fetch = require("node-fetch");
 const mysql = require("./connection").con;
 const cors = require("cors");
+const { resolveSoa } = require("dns/promises");
 const app = express();
 app.use(
   cors({
@@ -12,42 +13,54 @@ app.use(
 );
 app.use(express.json());
 const port = 3300;
-const modifySearchParamter = (searchParameter) => {
+const modifySearchParamter = (searchParameter, delimeter) => {
   var separatedArray = searchParameter.split(" ");
-  return separatedArray.join("%20");
+  return separatedArray.join(delimeter);
 };
-const scrapeData = (searchParameter,url,model) => {
+const scrapeData = (searchParameter, url, model,page_number=1) => {
   fetch(url)
-    .then((res) => res.text())
+    .then((res) => res.text()) 
     .then((body) => {
       const content = scrapy.extract(body, model);
-      let qry1='delete from newsarticles';
-      mysql.query(qry1,(err,result)=>{
-        if(err){
-          console.log(err)
+      let qry1 = `delete from newsarticles where NewsSearchParameter!='${searchParameter}'`;
+      mysql.query(qry1, (err, result) => {
+        if (err) {
+          return err
         }
-        else{
-          console.log("success deleting")
-        }
-      })
+      });
       content.news_articles.map((article) => {
-        let qry = `insert into newsarticles values (0,'${searchParameter}','${article.news_title.replaceAll(`'`,``)}','${"https://news.google.com/"+article.news_link}','${article.news_imageLink}')`;
+        let new_article_news_title=null;
+        if(article.news_title!==null){
+            new_article_news_title=article.news_title.replaceAll(
+              `'`,
+              ``
+            );
+        }
+        let qry = `insert into newsarticles values (0,'${searchParameter}','${new_article_news_title}','${article.news_link}','${
+          article.news_imageLink
+        }')`;
         mysql.query(qry, (err, result) => {
           if (err) {
-            console.log(err)
-            return
-          } else {
-            console.log("success")
-          }
+            return err;
+          } 
         });
       });
     });
+    return "success"
 };
 app.post("/", (req, res) => {
   const { searchParameter } = req.body;
-  const modifiedSearchParameter = modifySearchParamter(searchParameter);
-  const url = `https://news.google.com/search?q=${modifiedSearchParameter}&hl=en-IN&gl=IN&ceid=IN%3Aen`;
-  const model = {
+  const modifiedSearchParameter_google = modifySearchParamter(
+    searchParameter,
+    "%20"
+  );
+  const modifiedSearchParameter_bbc = modifySearchParamter(
+    searchParameter,
+    "+"
+  );
+  const url_google_news = `https://news.google.com/search?q=${modifiedSearchParameter_google}&hl=en-IN&gl=IN&ceid=IN%3Aen`;
+  const url_bbc_news = `https://www.bbc.co.uk/search?q=${modifiedSearchParameter_bbc}&d=news_gnl&page=`;
+  const model_google = {
     news_articles: [
       ".NiLAwe",
       {
@@ -56,10 +69,30 @@ app.post("/", (req, res) => {
         news_imageLink: "a figure img (src)",
       },
     ],
-
   };
-  scrapeData(searchParameter,url,model)
-  res.send("complete");
+  const model_bbc = {
+    news_articles: [
+      ".ett16tt0",
+      {
+        news_title: ".e3z3r3u0 .e1f5wbog7 .e1y4nx260 .eq5iqo00",
+        news_link: ".e3z3r3u0 .e1f5wbog7 .e1y4nx260 a (href)",
+        news_imageLink:
+          ".e3z3r3u0 .ehnfhlg4 .ehnfhlg2 .ehnfhlg3 .e16icw910  img (src)",
+      },
+    ],
+  };
+  let a=scrapeData(searchParameter, url_google_news, model_google);
+  if(a!=="success"){
+    res.send(a)
+  }
+  let b=""
+  for (let page_number_bbc = 1; page_number_bbc < 21; page_number_bbc++) {
+    b=scrapeData(searchParameter, url_bbc_news.concat(page_number_bbc), model_bbc,page_number_bbc);
+    if(b!=="success"){
+      res.send(b)
+    }
+  }
+  res.send("complete")
 });
 app.post("/getArticles", (req, res) => {
   let qry = `select * from newsarticles where NewsSearchParameter='${req.body.searchParameter}'`;
